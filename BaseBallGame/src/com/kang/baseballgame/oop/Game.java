ecc11b199d744076076c5ef6aa870006f8573707
KANG <KANG@DESKTOP-7Q5N3AD>
2019-10-24 17:07:17 +0900

    객체 지향 작업완료

--- BaseBallGame/src/com/kang/baseballgame/oop/Game.java
+++ BaseBallGame/src/com/kang/baseballgame/oop/Game.java
@@ -3,10 +3,17 @@ package com.kang.baseballgame.oop;
 public class Game {
 	public void startGame(int no_cnt) {
 		RandomNumbers rnb = new RandomNumbers(no_cnt);
-		UserNumbers unb = new UserNumbers(no_cnt);
 		Result result = new Result(no_cnt);
 		
-		
+		while(true) {
+			UserNumbers unb = new UserNumbers(no_cnt);
+			unb.setUserNumbers();
+			
+			if(Check.check(result, rnb, unb)) {
+				System.out.println("-- 게임 종료 --");
+				break;
+			}
+		}
 		
 	}
 }
--- BaseBallGame/src/com/kang/baseballgame/oop/Main.java
+++ BaseBallGame/src/com/kang/baseballgame/oop/Main.java
@@ -2,6 +2,7 @@ package com.kang.baseballgame.oop;
 
 public class Main {
 	public static void main (String[] args) {
-		
+		Game game = new Game();
+		game.startGame(3);
 	}
 }
